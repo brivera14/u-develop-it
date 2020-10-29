96bf3a781741c4491a04a15981cf194c1d7ffdf0
Bryan Rivera <rivera.bryan1406@gmail.com>
2020-10-29 16:06:05 -0400

    api endpoints routes updated to join the information of the parties to the candidates table'

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const db = new sqlite3.Database('./db/election.db', err => {
 // Get all candidates
 app.get("/api/candidates", (rep, res) => {
 
-    const sql = `SELECT * FROM candidates`;
+    const sql = `SELECT candidates.*, parties.names AS party_name FROM candidates LEFT JOIN parties ON candidates.party_id = parties.id`;
     params = [];
     db.all(sql, params, (err, rows) => {
         if (err) {
@@ -43,7 +43,7 @@ app.get("/api/candidates", (rep, res) => {
 
 // Get a single candidate
 app.get("/api/candidate/:id", (req, res) => {
-    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const sql = `SELECT candidates.*, parties.names AS party_name FROM candidates LEFT JOIN parties ON candidates.party_id = parties.id WHERE candidates.id = ?`;
     const params = [req.params.id];
     db.get(sql, params, (err, row) => {
         if (err) {
